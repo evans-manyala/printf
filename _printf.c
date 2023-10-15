bf661df40dfe93b1c046eb1d6c9622ed76613b36
evans-manyala <evans.manyala>
2023-10-15 03:44:59 -0700

    Adding _printf.c and _putchar.c

---
+++ _printf.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	convert_string m[] = {
+		{"%s", print_string}, {"%c", printf_char},
+	};
+
+	va_list args;
+	int x = 0, y, len = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[x] != '\0')
+	{
+		y = 13;
+		while (y >= 0)
+		{
+			if (m[j].id[0] == format[x] && m[y].id[1] == format[x + 1])
+			{
+				len += m[j].f(args);
+				x = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[x]);
+		len++;
+		x++;
+	}
+	va_end(args);
+	return (len);
+}
