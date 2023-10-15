1c797b54bfce10fb40af313c53202ab09500b107
evans-manyala <evans.manyala>
2023-10-15 04:12:06 -0700

    Adding Betty standard description

--- _putchar.c
+++ _putchar.c
@@ -1,3 +1,12 @@
+#include "main.h"
+
+/**
+ * _putchar - Prints char c to the standard output.
+ * @c: Char to be printed.
+ *
+ * Return: Upon success 1 otherwise -ve 1.
+ */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
