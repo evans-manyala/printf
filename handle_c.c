074bc64fde5a0d65f385ab2a3f9fb74a2d31c115
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-16 14:11:07 +0300

    Update handle_c.c

--- handle_c.c
+++ handle_c.c
@@ -10,8 +10,8 @@
 
 int handle_c(va_list lst, int *count)
 {
-	char c = va_arg(lst, int);
-	_putchar(c);
+	/*char c = va_arg(lst, int);*/
+	_putchar(va_arg(lst, int));
 	(*count)++;
 	return (1); /* Return 1 to indicate that one character has been processed */
 }
