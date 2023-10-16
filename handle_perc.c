0e2408df92bd0c1438535aedaba12acff97ba11b
evans-manyala <evans.manyala>
2023-10-16 14:56:02 -0700

    Adding logical error fix

--- handle_perc.c
+++ handle_perc.c
@@ -11,6 +11,9 @@ int handle_perc(va_list lst, int *count)
 {
 	(void)lst; /* Unused parameter (avoid compiler warnings) */
 
-	*count += _putchar('%'); /* Print '%' and increment the character count*/
+	_putchar('%');
+	(*count)++;
+
+	/*count += _putchar('%');  Print '%' and increment the character count*/
 	return (1); /*Return 1 to indicate that one character has been processed*/
 }
