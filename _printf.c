09df7ca7c61978f6dd23900326d884a8c62cb9f9
Jarvis Rwabuyongo <jarvisrwabuyongo45@gmail.com>
2023-10-17 18:59:18 +0300

    Stable code

--- _printf.c
+++ _printf.c
@@ -7,11 +7,8 @@
 
 int _printf(const char *format, ...)
 {
-	FormatSpecifier specifiers[] = {
-		{'s', handle_s},
-		{'c', handle_c},
-		{'%', handle_perc}
-};
+FormatSpecifier specifiers[] = {{ 's', handle_s },
+{'c', handle_c}, {'%', handle_perc}, {'d', handle_d}, {'i', handle_d}};
 
 	int count = 0;
 	size_t i;
