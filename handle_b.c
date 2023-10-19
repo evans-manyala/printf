dfee8b1b49c42333100733a6e4bcb8235821cfd3
evans-manyala <evans.manyala>
2023-10-18 17:05:23 -0700

    Adding Format specs

--- handle_b.c
+++ handle_b.c
@@ -2,16 +2,33 @@
 /**
  * handle_b - Function converts the unsigned int
  * argument into binary.
- * @lst: List of arguments
+ * @lst: List of arguments.
+ * @format: fornt specifications.
  * @count: Count value of the number of characters
  * Return: 1 if successful.
  */
 
-int handle_b(va_list lst, int *count)
+int handle_b(va_list lst, int *count, const char *format)
 {
+	int width, precision = 0;
 	unsigned int b = va_arg(lst, unsigned int);
 
-	unsigned_int_to_binary(b, count);
+	while (*format != '\0' && (*format < '0' || *format > '9'))
+	{
+		if (*format == '-')
+		{
+			width = -1;
+		}
+		else if (*format == '.')
+		{
+			precision = atoi(format + 1);
+			break;
+		}
+
+		format++;
+	}
+
+	print_decimal_integer(va_arg(lst, int), width, precision, count);
 
 	return (1);
 }
